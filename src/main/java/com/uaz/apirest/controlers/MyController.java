bae1271e8af8c942de207b4ec634220020f7d3c6
ElanTic <aplicacionesdejon@gmail.com>
2024-10-07 08:58:05 -0600

    agregado transformer csv

--- src/main/java/com/uaz/apirest/controlers/MyController.java
+++ src/main/java/com/uaz/apirest/controlers/MyController.java
@@ -1,13 +1,21 @@
-package controlers;
+package com.uaz.apirest.controlers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.uaz.apirest.gestor.transformers.CSVTransformer;
 
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class MyController {
+    @Autowired
+    CSVTransformer transformer;
 
     @GetMapping("/")
     public String index(Model model) {
@@ -18,5 +26,17 @@ public class MyController {
     @GetMapping("/partial")
     public String partialContent(Model model) {
         return "fragments :: contentFragment";
+    }
+
+    @PostMapping("/csv")
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            // Convert MultipartFile to a temporary file or InputStream for Spark processing
+            transformer.runEtlPipeline(file);
+            return "File uploaded and processed successfully!";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Failed to upload and process the file.";
+        }
     }
 }
