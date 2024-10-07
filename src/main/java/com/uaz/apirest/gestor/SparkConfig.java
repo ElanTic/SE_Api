2433870d5da600e053eaca3287a9bd53ae75d9e5
ElanTic <aplicacionesdejon@gmail.com>
2024-10-06 23:10:02 -0600

    agregado bean sparkSession

---
+++ src/main/java/com/uaz/apirest/gestor/SparkConfig.java
@@ -0,0 +1,16 @@
+package com.uaz.apirest.gestor;
+
+import org.apache.spark.sql.SparkSession;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SparkConfig {
+    @Bean
+    public SparkSession sparkSession() {
+        return SparkSession.builder()
+                .appName("se-api-gestor")
+                .master("local[*]")
+                .getOrCreate();
+    }
+}
