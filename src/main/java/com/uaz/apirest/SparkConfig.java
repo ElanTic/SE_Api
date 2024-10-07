c287747cf57d4fb9e63b848b99ccaada1aaf7f02
ElanTic <aplicacionesdejon@gmail.com>
2024-10-07 00:14:20 -0600

    jelp

--- src/main/java/com/uaz/apirest/SparkConfig.java
+++ src/main/java/com/uaz/apirest/SparkConfig.java
@@ -6,11 +6,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SparkConfig {
+
     @Bean
     public SparkSession sparkSession() {
         return SparkSession.builder()
-                .appName("se-api-gestor")
-                .master("local[*]")
+                .appName("apirest")
+                .config("spark.master", "local")
                 .getOrCreate();
     }
 }
